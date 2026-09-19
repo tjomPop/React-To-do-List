2cd1e3a2b653704e88ea796cbf65eb24bea7ba42
tjomPop <tjomsgithub@gmail.com>
2026-09-19 15:26:04 +0300

    Props and childrens

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -4,8 +4,12 @@ import Button from "./Button.jsx";
 const AddTaskForm = () => {
     return (
         <form className="todo__form">
-            <Field />
-            <Button/>
+            <Field
+                className="todo__field"
+                label="New Task Title"
+                id="new-task"
+            />
+            <Button type="submit">Add</Button>
         </form>
     )
 }
